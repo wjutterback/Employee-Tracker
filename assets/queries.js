ec668c787d7661a53d47daa762f053fbf7d130c3
Will Utterback <uwill08@gmail.com>
2021-02-26 00:22:19 -0600

    Fixed MySQL query errors

--- assets/queries.js
+++ assets/queries.js
@@ -21,23 +21,28 @@ exports.Department = Department;
 exports.Role = Role;
 
 // From DB Tables (to reference)
-// CREATE TABLE employee(
+// CREATE TABLE department(
 //   id INT AUTO_INCREMENT,
-//   first_name VARCHAR(30),
-//   last_name VARCHAR(30),
-//   PRIMARY KEY(id),
-//   FOREIGN KEY(role_id) REFERENCES role(id),
-//   FOREIGN KEY(manager_id) REFERENCES employee(id)
+//   name VARCHAR(30),
+//   PRIMARY KEY(id)
 //   );
 
 //   CREATE TABLE role(
 //   id INT AUTO_INCREMENT,
 //   title VARCHAR(30),
 //   salary DECIMAL,
+//   department_id INT,
+//   PRIMARY KEY(id),
 //   FOREIGN KEY(department_id) REFERENCES department(id)
 //   );
 
-//   CREATE TABLE department(
+//   CREATE TABLE employee(
 //   id INT AUTO_INCREMENT,
-//   name VARCHAR(30)
+//   first_name VARCHAR(30),
+//   last_name VARCHAR(30),
+//   role_id INT,
+//   manager_id INT,
+//   PRIMARY KEY(id),
+//   FOREIGN KEY(role_id) REFERENCES role(id),
+//   FOREIGN KEY(manager_id) REFERENCES employee(id)
 //   );
