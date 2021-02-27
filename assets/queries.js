7ab8ef1f5defd4623e70ad2f68e5fead89b334d4
Will Utterback <uwill08@gmail.com>
2021-02-26 18:29:05 -0600

    Had to change file structure/exports due to inquirer console.table display issues - will revert if I can fix the problem

--- assets/queries.js
+++ assets/queries.js
@@ -1,3 +1,5 @@
+const inquirer = require('./prompt');
+const questions = require('./questions');
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.host,
@@ -78,63 +80,6 @@ function addRole(role) {
   });
 }
 
-function addEmployee(employee) {
-  console.log('employee in queries', employee);
-  connection.query(
-    //TODO: this query will not be accurate/will not work if more than one manager role exists, needs a better query
-    `SELECT * FROM employee WHERE employee.first_name = '${employee.manager}' AND employee.role_id = '1'`,
-    (err, res) => {
-      if (err) throw err;
-      console.log(res);
-      connection.query(
-        'INSERT INTO employee SET ?',
-        {
-          first_name: employee.first_name,
-          last_name: employee.last_name,
-          role_id: employee.role_id,
-          manager_id: res[0].id,
-        },
-        (err, res) => {
-          if (err) {
-            return reject(err);
-          }
-          console.log(`${res.affectedRows} employee added`);
-        }
-      );
-    }
-  );
-}
-
-function viewEmployee(byDepartment, byManager) {
-  if (byDepartment === true) {
-    connection.query(
-      `SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', department.name AS 'Department' FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id`,
-      (err, res) => {
-        if (err) throw err;
-        console.log('');
-        console.table(res);
-      }
-    );
-  } else if (byManager === true) {
-    connection.query(
-      `SELECT employee1.first_name AS 'First Name', employee1.last_name AS 'Last Name', employee.first_name AS Manager FROM employee as employee1 INNER JOIN employee ON employee1.manager_id = employee.id;`,
-      (err, res) => {
-        if (err) throw err;
-        console.log('');
-        console.table(res);
-        console.log('');
-      }
-    );
-  } else {
-    connection.query('SELECT * FROM employee', (err, res) => {
-      if (err) throw err;
-      console.log('');
-      console.table(res);
-      console.log('');
-    });
-  }
-}
-
 function deleteEmployee() {
   connection.query('SELECT * FROM employee', (err, res) => {
     if (err) throw err;
@@ -145,8 +90,6 @@ function deleteEmployee() {
 }
 module.exports = {
   department: department,
-  addEmployee: addEmployee,
-  viewEmployee: viewEmployee,
   addRole: addRole,
   deleteEmployee: deleteEmployee,
 };
--- index.js
+++ index.js
@@ -2,13 +2,7 @@ require('dotenv').config();
 const inquirer = require('./assets/prompt');
 const questions = require('./assets/questions');
 const mysql = require('mysql2');
-const {
-  department,
-  addEmployee,
-  viewEmployee,
-  deleteEmployee,
-  addRole,
-} = require('./assets/queries');
+const { department, deleteEmployee, addRole } = require('./assets/queries');
 const C = require('./assets/constructors');
 
 const connection = mysql.createConnection({
@@ -37,35 +31,26 @@ async function selectionFunc() {
       case 'Add Employee':
         let employeeData = await inquirer(questions.addEmployee);
         const dept = new C.Department(employeeData.department);
-        let deptId = await department(dept, null);
-        console.log('deptId', deptId);
-        const role = new C.Role(
-          employeeData.role,
-          employeeData.salary,
-          deptId.toString()
-        );
+        let deptId = await department(dept);
+        const role = new C.Role(employeeData.role, employeeData.salary, deptId);
         let roleId = await addRole(role);
         const employee = new C.Employee(
           employeeData.fname,
           employeeData.lname,
-          roleId.toString(),
+          roleId,
           employeeData.manager
         );
         console.log(employee);
-        await addEmployee(employee);
-        selectionFunc();
+        addEmployee(employee);
         break;
       case 'View all Employees':
         viewEmployee();
-        selectionFunc();
         break;
       case 'View all Employees By Department':
         viewEmployee(true);
-        selectionFunc();
         break;
       case 'View all Employees By Manager':
         viewEmployee('', true);
-        selectionFunc();
         break;
       //TODO: Remove Employee
       case 'Remove Employee':
@@ -81,5 +66,67 @@ async function selectionFunc() {
     }
   } catch (error) {
     console.error(error);
+  }
+}
+
+function addEmployee(employee) {
+  console.log('employee in queries', employee);
+  connection.query(
+    //TODO: this query will not be accurate/will not work if more than one manager role exists, needs a better query
+    `SELECT * FROM employee WHERE employee.first_name = '${employee.manager}' AND employee.role_id = '1'`,
+    (err, res) => {
+      if (err) throw err;
+      console.log(res);
+      connection.query(
+        'INSERT INTO employee SET ?',
+        {
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          role_id: employee.role_id,
+          manager_id: res[0].id,
+        },
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          console.log(`${res.affectedRows} employee added`);
+          selectionFunc();
+        }
+      );
+    }
+  );
+}
+
+function viewEmployee(byDepartment, byManager) {
+  if (byDepartment === true) {
+    connection.query(
+      `SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', department.name AS 'Department' FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('');
+        console.table(res);
+        console.log('');
+        selectionFunc();
+      }
+    );
+  } else if (byManager === true) {
+    connection.query(
+      `SELECT employee1.first_name AS 'First Name', employee1.last_name AS 'Last Name', employee.first_name AS Manager FROM employee as employee1 INNER JOIN employee ON employee1.manager_id = employee.id;`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('');
+        console.table(res);
+        console.log('');
+        selectionFunc();
+      }
+    );
+  } else {
+    connection.query('SELECT * FROM employee', (err, res) => {
+      if (err) throw err;
+      console.log('');
+      console.table(res);
+      console.log('');
+      selectionFunc();
+    });
   }
 }
